f58555f0de2875866c0f4dd32ba482b2f7e6faca
rutugandhi <rutugandhi21@gmail.com>
2019-05-01 23:31:55 -0400

    Updated

---
+++ team-mangalyaan.py
@@ -0,0 +1,91 @@
+"""
+This script is the starter script that the user needs to run, which will set up
+the dataset, call the corresponding models that the user chooses, and write the
+results into the 'results' directory.
+---------------------------
+Author : Rutu Gandhi
+"""
+
+
+import argparse
+import subprocess
+from src.loan_status_prediction import Classifier
+from src.utils.preproc_data import Preprocessing
+from src.nmf import Nmf
+from src.sparsepca import sparse_pca
+from src.unet import UNet
+
+
+parser = argparse.ArgumentParser(description='Team mangalyaan : Fairness in Machine Learning')
+parser.add_argument('--model', dest='model', type=str, choices=['nmf', 'unet', 'sparsepca'],
+	default='logistic_regression', help='model to perform classification on a new dataset')
+parser.add_argument('--trainpath', dest='trainpath',type=str, default='../team-mangalyaan/loan_train.csv',
+	help='local path of csv file')
+parser.add_argument('--testpath', dest='testpath',type=str, default='../team-mangalyaan/loan_test.csv',
+	help='local path of csv file')
+parser.add_argument('--dataset', dest='dataset', type=str, default='COMPAS', choices=['COMPAS','Loan_status', 'Student'],
+	help='name dataset to try')
+parser.add_argument('--y_train', dest='y_train', type=str, default='Loan_Status', help='Name of target column')
+parser.add_argument('--postprocess', dest = 'postprocess', type=bool, default=False,
+	choices=['Equalize AP','Convert AP','Defer Threshold' ], help='soft classification to hard classification')
+parser.add_argument('--test', dest='test', type=bool, default=False,
+	choices=[True, False], help='to run the test suite')
+
+
+
+args = parser.parse_args()
+trainpath = args.trainpath
+testpath = args.testpath
+model = args.model
+dataset = args.dataset
+postprocess = args.postprocess
+
+
+
+cl = Classifier(dataset, trainpath, testpath)
+
+if test:
+	print('Beginning Test Suite...')
+	subprocess.call('python -m pytest', shell=True)
+else:
+    if dataset == 'COMPAS':
+	    if model == 'logistic_regression':
+            print("Classification not needed. Already done")
+            print("Proceeding to postprocess")
+    		if postprocess == 'Equalize_AP' :
+                command = 'Rscript'
+    			path_to_rscript = '../compas_equalize.R'
+
+            elif postprocess == 'Defer Threshold':
+                command = 'Rscript'
+    			path_to_rscript = '../compas_defer_threshold.R'
+
+            cmd = [command, path_to_rscript]
+            subprocess.checkout(cmd)
+    elif dataset == 'Loan_status':
+        if model == 'logistic_regression':
+            cl.run()
+            if postprocess == 'Equalize_AP' :
+                command = 'Rscript'
+    			path_to_rscript = '../loan_equalize.R'
+
+            elif postprocess == 'Defer Threshold':
+                command = 'Rscript'
+    			path_to_rscript = '../loan_defer_threshold.R'
+
+            cmd = [command, path_to_rscript]
+            subprocess.checkout(cmd)
+	elif dataset == 'Student':
+        if model == 'logistic_regression':
+            print("Classification not needed. Already done")
+            print("Proceeding to postprocess")
+            if postprocess == 'Equalize_AP' :
+                command = 'Rscript'
+    			path_to_rscript = '../student_equalize.R'
+
+            elif postprocess == 'Defer Threshold':
+                command = 'Rscript'
+    			path_to_rscript = '../student_defer_threshold.R'
+
+            cmd = [command, path_to_rscript]
+            subprocess.checkout(cmd)
--- tests/deferral_gender_test.R
+++ tests/deferral_gender_test.R
@@ -12,3 +12,6 @@ cex_factor = 1.6
 assertthat::assert_that(file.exists("./compas-scores-two-years.csv"))
 assertthat::is.readable("./compas-scores-two-years.csv")
 raw_data <- read.csv("./compas-scores-two-years.csv")
+
+assertr::verify(raw_data, has_all_names("raw_data", "age", "c_charge_degree", "race", "age_cat", "score_text", "sex", "priors_count"))
+
