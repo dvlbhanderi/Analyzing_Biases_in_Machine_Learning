a8ba50e448b9158d557cd49191ea46a8c50c1881
Shrinidhi Adke <43625263+shriadke@users.noreply.github.com>
2019-05-03 23:31:34 -0400

    Update test_data.R

---
+++ tests/test_data.R
@@ -0,0 +1,25 @@
+
+library(grid)
+library(gridExtra)
+library(dplyr)
+library(ggplot2)
+library(assertthat)
+library(assertr)
+# hyperparameter for plots
+cex_factor = 1.6
+
+# Load and clean the data selecting for the desired fields
+assertthat::assert_that(file.exists("./compas-scores-two-years.csv"))
+assertthat::is.readable("./compas-scores-two-years.csv")
+raw_data <- read.csv("./compas-scores-two-years.csv")
+
+#assertr::verify(raw_data, has_all_names("raw_data", "age", "c_charge_degree", "race", "age_cat", "score_text", "sex", "priors_count"))
+
+
+assertthat::assert_that(file.exists("./loan_test.csv"))
+assertthat::is.readable("./loan_test.csv")
+raw_data <- read.csv("./loan_test.csv")
+
+assertthat::assert_that(file.exists("./loan_train.csv"))
+assertthat::is.readable("./loan_train.csv")
+raw_data <- read.csv("./loan_train.csv")
