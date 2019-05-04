85bd57fa48b68497f2e9766e7d41493b10293112
Shrinidhi Adke <43625263+shriadke@users.noreply.github.com>
2019-05-03 23:31:02 -0400

    Update run_tests.R

--- tests/run_tests.R
+++ tests/run_tests.R
@@ -1,5 +1,9 @@
 library(testthat) 
 
-#source("C:\\Users\\shrin\\Documents\\GitHub\\team-mangalyaan\\team-mangalyaan\\src\\compas_two_threshold_gender.R")
 
-test_results <- test_dir("C:\\Users\\shrin\\Documents\\GitHub\\team-mangalyaan\\team-mangalyaan\\tests\\", reporter="summary")
+source("../src/compas_equalize_gender.R")
+source("../src/compas_two_threshold_race.R")
+source("../src/compas_equalize_race.R")
+
+source("../src/compas_two_threshold_gender.R")
+test_results <- test_dir("./", reporter="summary")
